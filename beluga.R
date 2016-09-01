8f350a6528801e502080509374af1469467f88e5
Matias Salibian Barrera <matias@stat.ubc.ca>
2016-09-01 09:59:22 -0700

    another example

--- beluga.R
+++ beluga.R
@@ -6,16 +6,22 @@ source('pen-s-functions.R')
 
 dd <- read.csv('whale.csv')
 
-y <- dd$Nursing1
-x <- dd$Period1
+y0 <- dd$Nursing1
+x0 <- dd$Period1
+n <- length(x0)
+x0 <- x0 / n
+
+set.seed(1234)
+ii <- sort(sample(n, 100, repl=FALSE))
+x <- x0[ii]
+y <- y0[ii]
 n <- length(x)
-x <- x / n
 
 plot(y ~ x, pch=19, cex=.7, col='gray')
 
 
 p <- 3
-num.knots <- max(5, min(floor(length(unique(x))/4), 10))
+num.knots <- 5 # max(5, min(floor(length(unique(x))/4), 5))
 knots <- quantile(unique(x),seq(0,1,length=num.knots+2))[-c(1,(num.knots+2))]
 xpoly <- rep(1,n)
 for (j in 1:p) xpoly <- cbind(xpoly,x^j)
@@ -24,7 +30,7 @@ xspline <- pmax(xspline, 0)^p
 X <- cbind(xpoly,xspline)
 D <- diag(c(rep(0,ncol(xpoly)),rep(1,ncol(xspline))))
 
-lambdas <- exp(seq(-10, 0, length=100))
+lambdas <- exp(seq(-20, 0, length=20))
 ll <- length(lambdas)
 
 tmp.ls <- pen.ls.gcv(y, X, D, lambdas)
@@ -39,6 +45,9 @@ lines(x, tmp.ls$yhat, lwd=2)
 cc <- 1.54764
 b <- .5
 
+# cc <- 3
+# b <- .24
+
 # Cubic polynomials
 NNN <- NN <- 100 # ??
 
@@ -46,7 +55,7 @@ tmp.m <- pen.m.rcv(y=y, X=X, N=NN, D=D, lambdas=lambdas, num.knots=num.knots, p=
 (opt.lam.m <- tmp.m$lam)
 gcvs.m <- tmp.m$gcv
 
-plot(y ~ x, main='Balloon data - subset 1', pch=19, col='gray', cex=.7)
+plot(y ~ x, pch=19, col='gray', cex=.7)
 lines(x, tmp.ls$yhat, lwd=2)
 lines(x, tmp.m$yhat, col="red", lwd=2)
 
@@ -55,7 +64,7 @@ tmp.s <- pen.s.rgcv(y, X, D, lambdas, num.knots, p, NN, cc, b, NNN)
 (opt.lam.s <- tmp.s$lam)
 gcvs.s <- tmp.s$gcv
 
-plot(y ~ x, main='Balloon data - subset 1', pch=19, col='gray', cex=.7)
+plot(y ~ x, pch=19, col='gray', cex=.7)
 lines(x, tmp.ls$yhat, lwd=2)
 lines(x, tmp.m$yhat, col="red", lwd=2)
 lines(x, tmp.s$yhat, col="blue", lwd=2)
