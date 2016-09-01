81ca22e474f5ecc6beba979bea372cd85917480d
Matias Salibian Barrera <matias@stat.ubc.ca>
2016-09-01 13:06:58 -0700

    another example

--- beluga.R
+++ beluga.R
@@ -6,7 +6,7 @@ source('pen-s-functions.R')
 
 dd <- read.csv('whale.csv')
 
-y0 <- dd$Nursing2 # 1
+y0 <- dd$Bouts2 # Nursing2 # 1
 x0 <- dd$Period1
 i0 <- !is.na(y0)
 x0 <- x0[i0]
@@ -14,20 +14,22 @@ y0 <- y0[i0]
 n <- length(x0)
 x0 <- x0 / n
 
-# set.seed(12)
-# ii <- sort(sample(n, 100, repl=FALSE))
-# x <- x0[ii]
-# y <- y0[ii]
-# n <- length(x)
 
-x <- x0
-y <- y0
+
+set.seed(123)
+ii <- sort(sample(n, 100, repl=FALSE))
+x <- x0[ii]
+y <- y0[ii]
+n <- length(x)
+
+# x <- x0
+# y <- y0
 
 plot(y ~ x, pch=19, cex=.7, col='gray')
 
 
 p <- 3
-num.knots <- max(5, min(floor(length(unique(x))/4), 35))
+num.knots <- max(5, min(floor(length(unique(x))/4), 15))
 knots <- quantile(unique(x),seq(0,1,length=num.knots+2))[-c(1,(num.knots+2))]
 xpoly <- rep(1,n)
 for (j in 1:p) xpoly <- cbind(xpoly,x^j)
@@ -36,13 +38,13 @@ xspline <- pmax(xspline, 0)^p
 X <- cbind(xpoly,xspline)
 D <- diag(c(rep(0,ncol(xpoly)),rep(1,ncol(xspline))))
 
-lambdas <- exp(seq(-10, 0, length=100))
+lambdas <- seq(1e-8, 3, length=100) # exp(seq(-20, 0, length=100))
 ll <- length(lambdas)
 
 tmp.ls <- pen.ls.gcv(y, X, D, lambdas)
 (tmp.ls$lam)
 
-plot(y ~ x, main='Balloon data - subset 1', pch=19, col='gray', cex=.7)
+plot(y ~ x, pch=19, col='gray', cex=.7)
 lines(x, tmp.ls$yhat, lwd=2)
 
 
@@ -50,7 +52,7 @@ lines(x, tmp.ls$yhat, lwd=2)
 cc <- 1.54764
 b <- .5
 
-# cc <- 3
+# cc <- .3
 # b <- .24
 
 # Cubic polynomials
